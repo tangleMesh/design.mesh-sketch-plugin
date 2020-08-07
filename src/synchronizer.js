d07252026e5c9f76be4ad0988c9a5c93b4b59ca3
Jeremias Ehnnle <jeremias.ehnle@aol.com>
2020-08-07 12:13:37 +0200

    Update synchronizer and manifest to add document-open handler

--- src/synchronizer.js
+++ src/synchronizer.js
@@ -4,11 +4,34 @@ import { checkSynchronization } from "./credentials";
 import { getPreferences, setPreferences } from "./storage.helper";
 
 
+export function downloadLatestVersion () {
+  // check, if document has been set up to sync
+  const documentSync = getPreferences (document.documentIdentifier ());
+  if (documentSync != "true") {
+    return;
+  }
 
-export function documentSaved (context) {
-  const document = (context.document || context.actionContext.document);
-  
-  // TODO: check, if document has been set up to sync
+  // Check, if user has been logged in with credentials
+  if (!checkSynchronization ()) {
+    return;
+  }
+
+  // read location of the currently saved document
+  const filePath = document.fileURL ().path ();
+  log (filePath);
+
+  // download new version of the document
+  // TODO: Download the latest version!
+  // upload (filePath)
+  //   .then (response => {
+  //     console.log (response);
+  //     UI.message ("Synchronized with remote! 🙌");
+  //   })
+  //   .catch (() => UI.message ("Error: Could not synchronize! Please check your internet connection. 😳"));
+}
+
+export function uploadLatestVersion (document) {
+  // check, if document has been set up to sync
   const documentSync = getPreferences (document.documentIdentifier ());
   if (documentSync != "true") {
     return;
@@ -32,6 +55,20 @@ export function documentSaved (context) {
     .catch (() => UI.message ("Error: Could not synchronize! Please check your internet connection. 😳"));
 }
 
+export function documentOpened (context) {
+  const document = (context.document || context.actionContext.document);
+  downloadLatestVersion (document);
+
+  setInterval (() => {
+    // TODO: initiate interval to poll for remote updates on the file, and download copy, if remote branch has been changed! (replace current local file and refresh ui)
+    // TODO: show message, that you switched branch and upload latest local copy before to the old branch!
+  }, 30000);
+}
+
+export function documentSaved (context) {
+  const document = (context.document || context.actionContext.document);
+  uploadLatestVersion (document);
+}
 
 export function enableDocumentSync (context) {
   // Check, if user has been logged in with credentials
@@ -42,6 +79,9 @@ export function enableDocumentSync (context) {
   const document = context.document;
   setPreferences (document.documentIdentifier (), "true");
   UI.message ("Enabled synchronization for this document! ⏯");
+
+  // Upload the actual document
+  documentSaved (context);
 }
 
 export function disableDocumentSync (context) {
